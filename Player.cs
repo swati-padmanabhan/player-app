a031406dd65ea7675f1b70344f22082dc89204ac
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-16 12:19:39 +0530

    initial commit of both files

---
+++ Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp.models
+{
+    internal class Player
+    {
+        static int DEFAULT_AGE = 18;
+        public int Id { get; }
+        public string Name { get; }
+        public int Age { get; }
+
+
+        public Player(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+
+        }
+
+        public Player(int id, string name) : this(id, name, DEFAULT_AGE)
+        { }
+
+        public static Player WhoIsElder(Player[] players)
+        {
+            Player elder = players[0];
+
+            foreach (var player in players)
+            {
+                if (player.Age > elder.Age)
+                {
+                    elder = player;
+                }
+            }
+
+            return elder;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using PlayerApp.models;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Player[] players = new Player[]{
+                new Player(101, "Swati", 30),
+                new Player(102, "Roshani", 40),
+                new Player(103, "Shweta", 20),
+                new Player(104, "Sampada", 35),
+                new Player(105, "Swamini", 25)
+            };
+
+            Player eldestPlayer = Player.WhoIsElder(players);
+            Console.WriteLine($"The oldest player in the group is: {eldestPlayer.Name} " +
+                $"& age is: {eldestPlayer.Age}");
+
+        }
+    }
+}
